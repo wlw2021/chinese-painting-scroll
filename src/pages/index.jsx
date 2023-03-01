60ae01939900304b8d3a9ef671e75ca8751d4dbb
“Luwei <zoe_wang@bestsign.com>
2023-03-01 21:40:11 +0800

    创建工程

---
+++ src/pages/index.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import "./index.less"
+
+const Pages = () => {
+    return (
+        <div className="layout-container">
+            <div className="Title">Traditional Chinese Painting Biography</div>
+            <div className="A-Overview"></div>
+            <div className="B-Features"></div>
+            <div className="C-Appreciation"></div>
+            <div className="D-LinkedData"></div>
+        </div>
+    )
+}
+
+export default Pages
